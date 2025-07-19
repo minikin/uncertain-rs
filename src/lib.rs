754e658a0aa357734d93a55ad484b8cb28a883fb
Oleksandr Prokhorenko <djminikin@gmail.com>
2025-07-19 13:13:49 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! # uncertain-rs
+//!
+//! A Rust library for uncertainty-aware programming, implementing the approach from
+//! "Uncertain<T>: A First-Order Type for Uncertain Data" by Bornholt, Mytkowicz, and McKinley.
